9b27e56e95f5b42f0d94ecca671504254e033aaf
ValeriaK38 <65389894+ValeriaK38@users.noreply.github.com>
2022-11-23 21:23:40 +0200

    Revert "- adding user"

--- src/main/java/chatApp/service/EmailSenderService.java
+++ src/main/java/chatApp/service/EmailSenderService.java
@@ -1,26 +1,6 @@
 package chatApp.service;
 
-import chatApp.repository.UserRepository;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
+interface EmailSenderService {
+    void sendVerificationEmail(String destinationEmail, String verificationCode, int id);
 
-@Service
-public class EmailSenderService {
-
-    private final JavaMailSender mailSender;
-    public EmailSenderService(JavaMailSender mailSender, UserRepository userRepository) {
-        this.mailSender = mailSender;
-    }
-
-    public void sendVerificationEmail(String destinationEmail, Long id, String url) {
-        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom("lera38lera@gmail.com");
-        simpleMailMessage.setTo(destinationEmail);
-        simpleMailMessage.setSubject("Verification email for chat-app");
-        String sid = Long.toString(id);
-        String html =  "<html><body><p>  Verify email on the link below </p>" + "<a href= '"+url+".html?id="+sid+"'>Verification page</a></body></html>";
-        simpleMailMessage.setText(html);
-        this.mailSender.send(simpleMailMessage);
-    }
 }
