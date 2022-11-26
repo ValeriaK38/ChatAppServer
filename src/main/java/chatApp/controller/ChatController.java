5de00130ae4da492f9cfe31f299cf19c39462c2a
Dalia <dalya1812@gmail.com>
2022-11-26 12:54:23 +0200

    fix getAll

--- src/main/java/chatApp/controller/ChatController.java
+++ src/main/java/chatApp/controller/ChatController.java
@@ -5,12 +5,11 @@ import chatApp.Entities.HelloMessage;
 import chatApp.Entities.RequestMessage;
 import chatApp.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-import java.sql.SQLDataException;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -34,5 +33,11 @@ public class ChatController {
     @RequestMapping(value = "create", method = RequestMethod.POST)
     public String createMessage(@RequestBody RequestMessage requestMessage){
         return chatService.addMessage(requestMessage).toString();
+    }
+
+
+    @RequestMapping(value = "/getAll", method = RequestMethod.GET)
+    public List<ChatMessage> getAllUsers() {
+        return chatService.getAllMesseges();
     }
 }
