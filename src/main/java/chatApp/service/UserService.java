c465f262a88f8c554aa5add66e3e15d1ca47f52a
Leon12215 <leon12215@gmail.com>
2022-11-26 18:38:39 +0200

    Admin mute/unmute backend/rest is now tested (integration)

--- src/main/java/chatApp/service/UserService.java
+++ src/main/java/chatApp/service/UserService.java
@@ -1,5 +1,6 @@
 package chatApp.service;
 
+import chatApp.Entities.Enums.UserType;
 import chatApp.Entities.User;
 import chatApp.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -28,6 +29,16 @@ public class UserService {
     public void muteUnmute(String adminNickName, String userNickName, String status) {
         User tempUser = userRepository.findByNickName(userNickName);
         User tempAdmin = userRepository.findByNickName(adminNickName);
+
+        if(adminNickName == userNickName){
+            throw new IllegalArgumentException("You cant mute/unmute yourself!");
+        }
+        if(tempUser.getUserType() == UserType.ADMIN){
+            throw new IllegalArgumentException("Cant mute or unmute admins.");
+        }
+        if(tempAdmin.getUserType() != UserType.ADMIN){
+            throw new IllegalArgumentException("Only admin can mute or unmute users.");
+        }
 
         if(status == "mute"){
             tempAdmin.adminMuteUser(tempUser);
--- src/test/java/chatApp/controller/UserControllerTest.java
+++ src/test/java/chatApp/controller/UserControllerTest.java
@@ -1,5 +1,6 @@
 package chatApp.controller;
 
+import chatApp.Entities.Enums.UserType;
 import chatApp.Entities.User;
 import chatApp.repository.UserRepository;
 import chatApp.service.UserService;
@@ -27,17 +28,33 @@ class UserControllerTest {
     @Autowired
     private static UserRepository userRepository;
 
+    private static User mutedUser;
+    private static User unmutedUser;
+    private static User adminUser;
+    private static User adminUser2;
     List<User> users;
 
     @BeforeEach
     public void setup() {
         users = new ArrayList<>();
+        mutedUser = new User.UserBuilder("muted@muted.com", "1234", "MutedUser").build();
+        unmutedUser = new User.UserBuilder("leon@test.com", "1234", "LeonTest").build();
+        adminUser = new User.UserBuilder("admin@test.com", "1234", "AdminTest").build();
+        adminUser = new User.UserBuilder("admin2@test.com", "1234", "AdminTest2").build();
+        userController.muteUnmute(adminUser.getNickName(),unmutedUser.getNickName(),"unmute");
+        userController.muteUnmute(adminUser.getNickName(),mutedUser.getNickName(),"mute");
     }
 
     @AfterEach
     public void cleanup() {
+        userController.muteUnmute(adminUser.getNickName(),unmutedUser.getNickName(),"unmute");
+        userController.muteUnmute(adminUser.getNickName(),mutedUser.getNickName(),"mute");
         users = null;
+        mutedUser = null;
+        unmutedUser = null;
+        adminUser = null;
     }
+
 
     @Test
     void Get_All_Users_Successfully() {
@@ -52,5 +69,141 @@ class UserControllerTest {
 
         System.out.println("The first user in the database is:");
         System.out.println(users.get(0));
+    }
+
+    @Test
+    public void Admin_Mutes_User_Successfully() {
+        System.out.println("-------- Test admin mutes user successfully --------");
+
+        //Given an admin and unmuted registered user exist
+
+        //When the admin mutes the unmuted user.
+        userController.muteUnmute(adminUser.getNickName(),unmutedUser.getNickName(),"mute");
+
+        //Then the user is now muted
+        User tempUser = userController.getUserByNickname(unmutedUser.getNickName());
+        assertEquals(tempUser.isMuted(), true);
+        System.out.println("The user is muted");
+    }
+
+    @Test
+    public void Admin_Unmutes_User_Successfully() {
+        System.out.println("-------- Test admin unmutes user successfully --------");
+
+        //Given an admin and a muted registered user exist
+
+        //When the admin unmutes the muted user.
+        userController.muteUnmute(adminUser.getNickName(),mutedUser.getNickName(),"unmute");
+
+        //Then the user is unmuted
+        User tempUser = userController.getUserByNickname(mutedUser.getNickName());
+        assertEquals(tempUser.isMuted(), false);
+        System.out.println("The user is unmuted:");
+    }
+
+    @Test
+    public void Admin_Tries_To_Mute_Other_Admin() {
+        System.out.println("-------- Test admin tries to mute other admin --------");
+
+        //Give there are two admins in the DB
+
+        //When an admin tries to mute another admin Then he fails
+        assertThrows(Exception.class, () -> userController.muteUnmute(adminUser.getNickName(),adminUser2.getNickName()
+        ,"mute"));
+
+        System.out.println("Admin cant mute another admin");
+    }
+
+    @Test
+    public void User_Tries_To_Mute_Other_User() {
+        System.out.println("-------- Test user tries to mute other user --------");
+
+        //Given two users exist
+
+
+        //When a user tries to mute another user Then he fails
+        assertThrows(Exception.class, () -> userController.muteUnmute(mutedUser.getNickName(),unmutedUser.getNickName()
+                ,"mute"));
+
+        System.out.println("User cant mute another user");
+    }
+
+    @Test
+    public void User_Tries_To_Unmute_Other_User() {
+        System.out.println("-------- Test user tries to unmute other user --------");
+
+        //Given two users exist one of them is muted
+
+
+        //When a user tries to unmute another user Then he fails
+        assertThrows(Exception.class, () -> userController.muteUnmute(mutedUser.getNickName(),unmutedUser.getNickName()
+                ,"unmute"));
+
+        System.out.println("User cant unmute another user");
+    }
+
+    @Test
+    public void Admin_Tries_To_Unmute_Other_Admin() {
+        System.out.println("-------- Test admin tries to unmute other admin --------");
+
+        //Given two admins exist
+
+        //When a admin tries to unmute another admin Then he fails
+        assertThrows(Exception.class, () -> userController.muteUnmute(adminUser.getNickName(),adminUser2.getNickName()
+                ,"unmute"));
+
+        System.out.println("Admin cant unmute another admin");
+    }
+
+    @Test
+    public void User_Tries_To_Mute_Himself() {
+        System.out.println("-------- Test user tries to mute himself --------");
+
+        //Given a user exist
+
+        //When a user tries to mute himself
+        assertThrows(Exception.class, () -> userController.muteUnmute(unmutedUser.getNickName(),unmutedUser.getNickName()
+                ,"mute"));
+
+        System.out.println("User cant mute himself");
+    }
+
+    @Test
+    public void Admin_Tries_To_Mute_Himself() {
+        System.out.println("-------- Test admin tries to mute himself --------");
+
+        //Given a admin exist
+
+        //When a admin tries to mute himself
+        assertThrows(Exception.class, () -> userController.muteUnmute(adminUser.getNickName(),adminUser.getNickName()
+                ,"mute"));
+
+        System.out.println("Admin cant mute himself");
+    }
+
+    @Test
+    public void User_Tries_To_Unmute_Himself() {
+        System.out.println("-------- Test user tries to unmute himself --------");
+
+        //Given  a muted user exist
+
+        //When a user tries to unmute himself
+        assertThrows(Exception.class, () -> userController.muteUnmute(mutedUser.getNickName(),mutedUser.getNickName()
+                ,"unmute"));
+
+        System.out.println("User cant unmute himself");
+    }
+
+    @Test
+    public void Admin_Tries_To_Unmute_Himself() {
+        System.out.println("-------- Test user tries to unmute himself --------");
+
+        //Given admin exist
+
+        //When an admin tries to unmute himself
+        assertThrows(Exception.class, () -> userController.muteUnmute(adminUser.getNickName(),adminUser.getNickName()
+                ,"unmute"));
+
+        System.out.println("Admin cant unmute himself");
     }
 }
